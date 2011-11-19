f58c2d27228f2c3ab3ad1b21bf0935747b58b52d
netchip <vanderpoel24@gmail.com>
2011-11-19 18:29:31 +0100

    Initial commit

---
+++ products/common.mk
@@ -0,0 +1,27 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rommanager.developerid=ICS_build_by_team_ICS
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Used by BusyBox
+KERNEL_MODULES_DIR:=/system/lib/modules
+
+# Tiny toolbox
+TINY_TOOLBOX:=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Required, keyboard
+PRODUCT_PACKAGES += LatinIME
+
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo ics_galaxys2-eng
+add_lunch_combo ics_maguro-eng
+add_lunch_combo ics_panda-eng
